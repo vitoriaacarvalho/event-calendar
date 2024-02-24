1fb4304fe765e8a30137cc2cf7126add97c5dbd0
vitoria <vitoriabcarvalhocorrea@gmail.com>
2024-02-24 13:36:03 -0300

    remove unused imports

--- src/main/java/com/vits/EventCalendar/endpoints/RegisterController.java
+++ src/main/java/com/vits/EventCalendar/endpoints/RegisterController.java
@@ -2,15 +2,12 @@ package com.vits.EventCalendar.endpoints;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vits.EventCalendar.dtos.RegisterDTO;
-import com.vits.EventCalendar.models.User;
-import com.vits.EventCalendar.repositories.UserRepository;
 import com.vits.EventCalendar.services.AuthenticationService;
 
 import jakarta.validation.Valid;
