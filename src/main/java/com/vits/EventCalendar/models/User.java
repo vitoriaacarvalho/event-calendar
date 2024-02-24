112844e7cb5bab775698f7d35c358b5770b0ea31
vitoria <vitoriabcarvalhocorrea@gmail.com>
2024-02-24 12:09:12 -0300

    clean default unused comments from @override methods

--- src/main/java/com/vits/EventCalendar/models/User.java
+++ src/main/java/com/vits/EventCalendar/models/User.java
@@ -45,27 +45,22 @@ public class User implements UserDetails{
 	
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return login;
 	}
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 }
